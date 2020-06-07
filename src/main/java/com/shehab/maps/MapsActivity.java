fee5c4a9ed7baf011301a24b5066ec349a5f444e
Shehab Syed <45420365+shehab8@users.noreply.github.com>
2020-06-07 13:11:25 +0200

    Add files via upload

---
+++ src/main/java/com/shehab/maps/MapsActivity.java
@@ -0,0 +1,68 @@
+package com.shehab.maps;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        // Add a marker in Sydney and move the camera
+        LatLng kea = new LatLng(55.7037834,12.5368317);
+        mMap.addMarker(new MarkerOptions().position(kea).title("Kea School"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kea, 17));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                //Creating Marker
+                MarkerOptions markerOptions = new MarkerOptions();
+                // Set Marker position
+                markerOptions.position(latLng);
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                // Clear the Previously click position
+                mMap.clear();
+                //Zoom the Marker
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
+                //Add Marker on Map
+                mMap.addMarker(markerOptions);
+            }
+        });
+    }
+
+
+
+}
